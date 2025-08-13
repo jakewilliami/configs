ea6ada289f060b9b0f99857088f37a3d85a356ce
Jake Ireland <jakewilliami@icloud.com>
2025-08-13 21:24:22 +1200

    Update to latest version of Elpaca
    Closes #7
    
    Recommendation by @progfolio:
    
    > The installer is intended to be updated manually.
    >
    > Elisp could be written to do it automatically, but that's a non-goal
    > of mine for two main reasons:
    >   1. I don't think packages should alter the user's `init.el` file.
    >   2. I want to encourage users to review the installer prior to
    >      running it.

--- src/dotfiles/dot_emacs.d/init.el
+++ src/dotfiles/dot_emacs.d/init.el
@@ -42,7 +42,7 @@
 ;;   github.com/progfolio/elpaca/issues/151#issuecomment-2629560507
 ;;   github.com/progfolio/elpaca/issues/447
 ;;   github.com/progfolio/elpaca/blob/f0bbcec4/doc/manual.md#lock-files
-(defvar elpaca-installer-version 0.8)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))

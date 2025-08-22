17e5bb25cf0bc4f50a5d42312c2d60fe514e8858
Jake Ireland <jakewilliami@icloud.com>
2025-08-22 19:44:25 +1200

    Require absolute path for Elpaca lock file
    I figured it would default to the path relative to the init file, but
    I don't think that was working.
    
    Addresses #7

--- src/dotfiles/dot_emacs.d/init.el
+++ src/dotfiles/dot_emacs.d/init.el
@@ -46,7 +46,7 @@
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(setq elpaca-lock-file "elpaca.lock.eld")
+(setq elpaca-lock-file "~/.emacs.d/elpaca.lock.eld")
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
